ea68c9a35453eedff3ade9388cd3f3b5e08d3d60
Bruce Ruffin <bruffin1221@gmail.com>
2019-08-02 21:10:01 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(languages)
+  i=0
+  computer=[]
+  while i<ilanguages.length
+    computer<<
+    yield(languages[i])
+    i+=1
+  end
+  computer
+end
 
-
+my_collect(["ruby","javascript","python","objective-c"]) do |lang|
+  lang.upcase
+end
+  
